908f48456e9823405bcdfeb197b1f10afb879fb1
Stuart Turner <stuart@turner-isageek.com>
2022-12-29 19:59:32 -0600

    Add db initialization code

---
+++ Database/DbContext.InitializeDatabase.cs
@@ -0,0 +1,104 @@
+﻿using System.Reflection;
+using System.Text.RegularExpressions;
+using LinqToDB;
+using LinqToDB.Data;
+using MakingCents.Database.Models;
+using Microsoft.Extensions.Logging;
+
+namespace MakingCents.Database;
+
+public partial class DbContext : DataConnection
+{
+	public void InitializeDatabase()
+	{
+		LogInitializingDb();
+
+		CommandTimeout = 600;
+
+		EnsureVersionHistoryExists();
+
+		RunChangeScripts();
+
+		LogDbInitialized();
+	}
+
+	#region Main Functions
+	private void EnsureVersionHistoryExists() =>
+		// script does validation; always run script
+		ExecuteScript("00.VersionHistory.sql");
+
+	private void RunChangeScripts()
+	{
+		var scripts = GetEmbeddedScripts();
+		var executedScripts = VersionHistories
+			.Select(s => s.SqlFile)
+			.ToList();
+
+		var scriptsToRun = scripts
+			.Except(executedScripts, StringComparer.OrdinalIgnoreCase)
+			.Except(new[] { "00.VersionHistory.sql", }, StringComparer.OrdinalIgnoreCase)
+			.OrderBy(s => s)
+			.ToList();
+
+		foreach (var s in scriptsToRun)
+		{
+			using (var ts = BeginTransaction())
+			{
+				ExecuteScript(s);
+				ts.Commit();
+			}
+
+			this.Insert(
+				new VersionHistory()
+				{
+					SqlFile = s,
+					Timestamp = DateTime.Now,
+				});
+		}
+	}
+	#endregion
+
+	#region Execute Script
+	[GeneratedRegex("^go\\r?$", RegexOptions.IgnoreCase | RegexOptions.Multiline | RegexOptions.Compiled, "en-US")]
+	private static partial Regex SqlBlockRegex();
+
+	private void ExecuteScript(string scriptName)
+	{
+		try
+		{
+			var script = EmbeddedResource.GetContent(scriptName);
+			foreach (var b in SqlBlockRegex().Split(script).Where(s => !string.IsNullOrWhiteSpace(s)))
+				this.Execute(b);
+		}
+		catch (Exception ex)
+		{
+			LogUnableToRunScript(ex, scriptName);
+			throw;
+		}
+	}
+	#endregion
+
+	#region Embedded Scripts
+	private const string ResourcePrefix = "MakingCents.Database.Scripts.";
+	private static IList<string> GetEmbeddedScripts() =>
+		Assembly.GetExecutingAssembly()
+			.GetManifestResourceNames()
+			.Where(s => Path.GetExtension(s).Equals(".sql", StringComparison.OrdinalIgnoreCase))
+			.Select(s => s.Replace(ResourcePrefix, "", StringComparison.OrdinalIgnoreCase))
+			.ToList();
+
+	#endregion
+
+	#region Logging
+
+	[LoggerMessage(Level = LogLevel.Error, Message = "Unable to run script '{ScriptName}'.")]
+	private partial void LogUnableToRunScript(Exception ex, string scriptName);
+
+	[LoggerMessage(Level = LogLevel.Information, Message = "Initializing MakingCents DB")]
+	private partial void LogInitializingDb();
+
+	[LoggerMessage(Level = LogLevel.Information, Message = "MakingCents DB Initialized")]
+	private partial void LogDbInitialized();
+
+	#endregion
+}
---
+++ Database/DbContext.cs
@@ -0,0 +1,14 @@
+﻿using LinqToDB.Data;
+using Microsoft.Extensions.Logging;
+
+namespace MakingCents.Database;
+
+public partial class DbContext : DataConnection
+{
+	private readonly ILogger<DbContext> _logger;
+
+	public DbContext(ILogger<DbContext> logger)
+	{
+		_logger = logger;
+	}
+}
