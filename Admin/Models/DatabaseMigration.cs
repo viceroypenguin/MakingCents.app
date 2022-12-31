eccb4c19c4cf38baf8bd94d64fc6784aa5c97d22
Stuart Turner <stuart@turner-isageek.com>
2022-12-31 08:49:27 -0600

    Add first Service Layer project

---
+++ Admin/Models/DatabaseMigration.cs
@@ -0,0 +1,6 @@
+﻿namespace MakingCents.Admin.Models;
+public record DatabaseMigration(
+	int VersionHistoryId,
+	string SqlFile,
+	DateTimeOffset ApplicationTime,
+	TimeSpan ExecutionTime);
---
+++ Admin/Services/DatabaseService.cs
@@ -0,0 +1,24 @@
+﻿using LinqToDB;
+using MakingCents.Admin.Models;
+using MakingCents.Database;
+
+namespace MakingCents.Admin.Services;
+
+public class DatabaseService
+{
+	private readonly DbContext _context;
+
+	public DatabaseService(DbContext context)
+	{
+		_context = context;
+	}
+
+	public async Task<List<DatabaseMigration>> GetVersionHistories() =>
+		await _context.VersionHistories
+			.Select(vh => new DatabaseMigration(
+				vh.VersionHistoryId,
+				vh.SqlFile,
+				vh.ExecutionStart,
+				vh.ExecutionEnd - vh.ExecutionStart))
+			.ToListAsync();
+}
