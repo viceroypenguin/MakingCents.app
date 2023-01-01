b90d8efc4c8006f322edce240a09b961c0182f92
Stuart Turner <stuart@turner-isageek.com>
2023-01-01 17:08:48 -0600

    v1 of Web API

---
+++ Source/Web/Controllers/Admin/DatabaseController.cs
@@ -0,0 +1,22 @@
+﻿using MakingCents.Admin.Models;
+using MakingCents.Admin.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MakingCents.Web.Controllers.Admin;
+
+[ApiController]
+[Route("~/api/admin/database")]
+public sealed class DatabaseController : ControllerBase
+{
+	private readonly DatabaseService _databaseService;
+
+	public DatabaseController(
+		DatabaseService databaseService)
+	{
+		_databaseService = databaseService;
+	}
+
+	[HttpGet("migrations")]
+	public async Task<IEnumerable<DatabaseMigration>> GetDatabaseMigrations() =>
+		await _databaseService.GetVersionHistories();
+}
---
+++ Source/Web/Program.cs
@@ -0,0 +1,90 @@
+﻿using DryIoc.Microsoft.DependencyInjection;
+using Serilog;
+using Serilog.Exceptions.Core;
+using Serilog.Exceptions.Destructurers;
+using Serilog.Exceptions.MsSqlServer.Destructurers;
+using Serilog.Exceptions.Refit.Destructurers;
+using Serilog.Exceptions;
+using MakingCents.Database;
+using Serilog.Events;
+
+#pragma warning disable CA1031 // Do not catch general exception types
+#pragma warning disable CA1852 // Type can be sealed because it has no subtypes in its containing assembly and is not externally visible
+
+Log.Logger = new LoggerConfiguration()
+	.WriteTo.Console(formatProvider: null)
+	.CreateBootstrapLogger();
+
+try
+{
+	var builder = WebApplication.CreateBuilder(args);
+
+	builder.Host.UseServiceProviderFactory(
+		new DryIocServiceProviderFactory());
+
+	builder.Host.UseSerilog((ctx, lc) => lc
+		.ReadFrom.Configuration(ctx.Configuration)
+		.Enrich.WithExceptionDetails(new DestructuringOptionsBuilder()
+			.WithDefaultDestructurers()
+			.WithDestructurers(new IExceptionDestructurer[]
+			{
+				new SqlExceptionDestructurer(),
+				new ApiExceptionDestructurer(),
+			})));
+
+	builder.Services.AutoRegisterFromCommon();
+	builder.Services.AutoRegisterFromDatabase();
+
+	builder.Services.AutoRegisterFromAdmin();
+
+	builder.Services.Configure<DbContextOptions>(
+		builder.Configuration.GetSection("DbContextOptions"));
+
+	builder.Services.AddControllers();
+
+	builder.Services.AddEndpointsApiExplorer();
+	builder.Services.AddSwaggerGen();
+
+	var app = builder.Build();
+
+	using (var scope = app.Services.CreateScope())
+	{
+		var db = scope.ServiceProvider.GetRequiredService<DbContext>();
+		db.InitializeDatabase();
+	}
+
+	app.UseSwagger();
+	app.UseSwaggerUI();
+
+	app.UseHttpsRedirection();
+
+	app.UseStaticFiles();
+
+	app.UseAuthorization();
+
+	app.MapControllers();
+
+	app.UseSerilogRequestLogging(o =>
+	{
+		o.GetLevel = static (httpContext, _, _) =>
+			httpContext.Response.StatusCode >= 500 ? LogEventLevel.Error : LogEventLevel.Information;
+
+		o.EnrichDiagnosticContext = static (diagnosticContext, httpContext) =>
+		{
+			diagnosticContext.Set("User", httpContext.User?.Identity?.Name);
+			diagnosticContext.Set("RemoteIP", httpContext.Connection.RemoteIpAddress);
+			diagnosticContext.Set("ConnectingIP", httpContext.Request.Headers["CF-Connecting-IP"]);
+		};
+	});
+
+	app.Run();
+}
+catch (Exception ex)
+{
+	Log.Fatal(ex, "unhandled exception");
+}
+finally
+{
+	Log.Information("Shut down complete");
+	Log.CloseAndFlush();
+}
