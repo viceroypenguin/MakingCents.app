4066a972ca80b5a171c5e5bbe53aca35e18ab6af
Stuart Turner <stuart@turner-isageek.com>
2023-01-01 11:32:02 -0600

    Add Admin/Database Unit Test

---
+++ Tests/Tests.Admin/AdminDiFixture.cs
@@ -0,0 +1,14 @@
+﻿using MakingCents.Admin.Services;
+using MakingCents.Tests.Common;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MakingCents.Admin.Tests;
+
+public sealed class AdminDiFixture : DiFixture
+{
+	protected override void RegisterTypes(IServiceCollection services) =>
+		services.AutoRegisterFromAdmin();
+
+	public DatabaseService DatabaseService =>
+		ServiceProvider.GetRequiredService<DatabaseService>();
+}
---
+++ Tests/Tests.Admin/DatabaseServiceTests.cs
@@ -0,0 +1,36 @@
+﻿using CommunityToolkit.Diagnostics;
+using MakingCents.Database;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MakingCents.Admin.Tests;
+
+public sealed class DatabaseServiceTests : IClassFixture<AdminDiFixture>, IDisposable
+{
+	private readonly AdminDiFixture _diFixture;
+	private readonly IServiceScope _scope;
+
+	public DatabaseServiceTests(AdminDiFixture diFixture)
+	{
+		Guard.IsNotNull(diFixture);
+
+		_diFixture = diFixture;
+		_scope = _diFixture.ServiceProvider.CreateScope();
+	}
+
+	public void Dispose() => _scope.Dispose();
+
+	[Fact]
+	public async Task AllScriptsHaveBeenRun()
+	{
+		var service = _diFixture.DatabaseService;
+		var runScripts = await service.GetVersionHistories();
+
+		var dbAssembly = typeof(DbContext).Assembly;
+		var allScripts = dbAssembly.GetManifestResourceNames()
+			.Where(n => Path.GetExtension(n) == ".sql")
+			.Select(n => n.Replace("MakingCents.Database.Scripts.", "", StringComparison.OrdinalIgnoreCase))
+			.ToHashSet();
+
+		Assert.True(allScripts.SetEquals(runScripts.Select(r => r.SqlFile)));
+	}
+}
